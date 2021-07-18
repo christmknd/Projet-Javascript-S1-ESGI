107df6d66e9dbf8f9a5c3cf8bfb5ebef54155fba
Lamtaoui <77679054+Lamtaoui@users.noreply.github.com>
2021-07-18 03:31:57 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import Animal from "./animal.js";
+const root = document.getElementById("root");
+
+
+class Lign1 extends Component {
+    render() {
+        myelements, document.getElementById('root')
+    }
+}
+var myelements;
+var animal = new Animal();
+animal.initializeAnimal({type: {name: "chien"}});
+if (animal.shouldUpdate()){
+    display("chien");
+}else{
+    display("chat");
+}
+String.interpolate = function (animal) {
+    return "Type d'animal : ".animal.prop_access;
+};
+function display(type){
+    const machaine = "Type d'animal: {{ type.name }}";
+    machaine.interpolate(animal);
+    myelements=(
+        <p>${machaine}</p>
+    );
+}
