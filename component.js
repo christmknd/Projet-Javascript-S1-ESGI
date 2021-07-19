44bd400de157da401647cb0d09c263a35f2c87ef
christ <christmokonda@gmail.com>
2021-07-19 16:06:59 +0200

    [EDIT] Component.js

--- component.js
+++ component.js
@@ -7,23 +7,45 @@ export default class Component {
         this.props = props;
     }
 
-    receiveData(){
+    initializeComponent = function (data) {
+        if(this.type_check()){
+            this.prevState = this.state;
+            this.state = data;
 
-    }
+        }else{
+            console.log("check your data validity");
+        }
+    };
 
-    needUpdate(){
-    }
-
-    shouldUpdate(){
-
-    }
+    shouldUpdate = function () {
+        return (
+            this.prevState.state !== this.state
+        );
+    };
 
     propAcess(){
+        let path=type;
+        let obj=this;
+        const fullPath = path.split('.');
+        const partOfPath = [];
+        for (let part of fullPath) {
+            partOfPath.push(part);
+            if (obj[part] === undefined) {
+                console.log(partOfPath.join('.')+ " not exist.");
+                return;
+            }
+            obj = obj[part];
+    };
 
-    }
-
-    typeCheck(){
-
+    typeCheck()
+        {
+            for (const conf in this.state) {
+                switch (conf) {
+                    case 'name': return true;
+                    default: return false;
+                }
+            }
+        }
     }
 
 
