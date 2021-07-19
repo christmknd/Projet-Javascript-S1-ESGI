9c4d5e4c35fd72336d27234fabbe72154b20dd49
christ <christmokonda@gmail.com>
2021-07-19 17:06:36 +0200

    [EDIT] Component.js

--- component.js
+++ component.js
@@ -7,50 +7,15 @@ export default class Component {
         this.props = props;
     }
 
-    initializeComponent = function (data) {
-        if(this.type_check()){
-            this.prevState = this.state;
-            this.state = data;
 
-        }else{
-            console.log("check your data validity");
-        }
+    shouldUpdate() {
+        return JSON.stringify(this.prevState) !== JSON.stringify(this.state);
 
     };
 
 
-    shouldUpdate = function () {
-        return (
-            this.prevState.state !== this.state
-        );
-    };
-
-    propAcess(){
-        let path=type;
-        let obj=this;
-        const fullPath = path.split('.');
-        const partOfPath = [];
-        for (let part of fullPath) {
-            partOfPath.push(part);
-            if (obj[part] === undefined) {
-                console.log(partOfPath.join('.')+ " not exist.");
-                return;
-            }
-            obj = obj[part];
-    };
-
-    typeCheck()
-        {
-            for (const conf in this.state) {
-                switch (conf) {
-                    case 'name': return true;
-                    default: return false;
-                }
-            }
-        }
+    render(){
+        throw new Error("Component must be implemented !");
     }
-
-
-
 
 }
