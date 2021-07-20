6aa040b6e541fad661c1afb9979c0893539e49d4
LAMTAOUI <mlamtaoui1@myges.fr>
2021-07-20 19:54:08 +0200

    Architecture du projet

---
+++ src/outils/generateStructure.js
@@ -0,0 +1,33 @@
+const generateStructure = (struct) => {
+    let elem;
+    if (typeof struct.type === "string") {
+        elem = document.createElement(struct.type);
+
+        if (struct.children) {
+            for (let child of struct.children) {
+                const nodeElement =
+                    typeof child === "string"
+                        ? document.createTextNode(child)
+                        : generateStructure(child);
+                elem.appendChild(nodeElement);
+            }
+        }
+
+        if (struct.attributes) {
+            for (let attr in struct.attributes) {
+                if (attr.startsWith("on")) {
+                    elem.addEventListener(
+                        attr.replace(/^on/, ""),
+                        struct.attributes[attr]
+                    );
+                } else {
+                    elem.setAttribute(attr, struct.attributes[attr]);
+                }
+            }
+        }
+    } else {
+        elem = generateStructure(/* ... */);
+    }
+    return elem;
+};
+export default generateStructure;
