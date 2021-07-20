788aa93b8833b7cbf03bc9318fcc2510b5961fb9
Belkacem <morsi@MacBook-Pro-van-morsi.local>
2021-07-20 22:10:16 +0200

    Page FileReader

--- src/pages/FIleReader.js
+++ src/pages/FIleReader.js
@@ -1,14 +1,9 @@
 import createElement from "../outils/createElement";
-export default class HomeScreen{
+export default class FileReader{
     struct = {};
     constructor() {
         this.struct= createElement("ul", {}, [
             createElement("li", {}, ["Page1"]),
-            createElement("ul", {}, [
-                createElement("li", {}, ["sous-page1"]),
-                createElement("li", {}, ["sous-Page2"]),
-                createElement("li", {}, ["sous-page3"]),
-            ]),
             createElement("li", {}, ["Page2"]),
             createElement("li", {}, ["Page3"]),
         ]);
